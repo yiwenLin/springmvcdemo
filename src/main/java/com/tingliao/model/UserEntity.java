faf9cea4e118c63c0e69aedc64f8e3337cdb9139
Administrator <1030593409@qq.com>
2017-07-11 16:37:03 +0800

    添加用户管理

---
+++ src/main/java/com/tingliao/model/UserEntity.java
@@ -0,0 +1,103 @@
+package com.tingliao.model;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+/**
+ * Created by Administrator on 2017/7/10.
+ */
+@Entity
+@Table(name = "user", schema = "test", catalog = "")
+public class UserEntity {
+    private int id;
+    private String nickname;
+    private String password;
+    private String firstName;
+    private String lastname;
+    private Collection<BlogEntity> blogByBlogId;
+
+    @Id
+    @Column(name = "id", nullable = false)
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "nickname", nullable = false, length = 45)
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    @Basic
+    @Column(name = "password", nullable = false, length = 45)
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Basic
+    @Column(name = "first_name", nullable = true, length = 45)
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Basic
+    @Column(name = "lastname", nullable = true, length = 45)
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserEntity that = (UserEntity) o;
+
+        if (id != that.id) return false;
+        if (nickname != null ? !nickname.equals(that.nickname) : that.nickname != null) return false;
+        if (password != null ? !password.equals(that.password) : that.password != null) return false;
+        if (firstName != null ? !firstName.equals(that.firstName) : that.firstName != null) return false;
+        if (lastname != null ? !lastname.equals(that.lastname) : that.lastname != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (nickname != null ? nickname.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
+        return result;
+    }
+
+    @OneToMany(mappedBy = "userByUserId")
+    public Collection<BlogEntity> getBlogByBlogId() {
+        return blogByBlogId;
+    }
+
+    public void setBlogByBlogId(Collection<BlogEntity> blogByBlogId) {
+        this.blogByBlogId = blogByBlogId;
+    }
+}
